02de1defff72818d31b0dd2580791d4dd4faef93
Guilherme Santos <santosguilhermefk@gmail.com>
2022-02-23 23:50:07 -0300

    add styled-components

--- styles/home.style.js
+++ styles/home.style.js
@@ -2,5 +2,24 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 0 2rem;
+  width: 100vw;
 `;
-export const Main = styled.div``;
+export const Title = styled.h1`
+  text-align: center;
+  font-size: 3rem;
+  color: #e50914;
+`;
+export const List = styled.div`
+  margin-top: 5px;
+`;
+export const MoviesList = styled.ul`
+  display: flex;
+  li {
+    margin: 10px;
+  }
+`;
+export const Buttons = styled.div`
+  display: flex;
+`;
+export const ButtonNext = styled.button``;
+export const ButtonPrev = styled.button``;
